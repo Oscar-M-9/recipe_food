e41853db4a4b674b5360ce2fa1dcb629ebc698ce
Oscar-M-9 <oc6781343@gmail.com>
2024-09-24 10:58:04 -0500

    add: adding a constant

--- lib/app/config/utils/constants.dart
+++ lib/app/config/utils/constants.dart
@@ -4,4 +4,5 @@ abstract class Constants {
   /// cosntantes del enviroment
   static String supabaseUrl = dotenv.get("SUPABASE_URL");
   static String supabaseKey = dotenv.get("SUPABASE_KEY");
+  static String googleWebClientId = dotenv.get("GOOGLE_WEB_CLIENT_ID");
 }
